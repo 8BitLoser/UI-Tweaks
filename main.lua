d010b856b793f6b986612e7d6096eb32cbe0dd77
BeefStranger <brad.bingham@live.com>
2024-11-22 19:30:55 -0700

    Restructure QuickEscape/Hotkeys

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
-local cfg = require("BeefStranger.UI Tweaks.config")
-
 local modules = {
     "BeefStranger.UI Tweaks.common",
     "BeefStranger.UI Tweaks.config",
+    "BeefStranger.UI Tweaks.hotkeys",
     "BeefStranger.UI Tweaks.ID",
+    "BeefStranger.UI Tweaks.quickEscape",
+
     "BeefStranger.UI Tweaks.menu.HelpMenu",
     "BeefStranger.UI Tweaks.menu.MenuBarter",
     "BeefStranger.UI Tweaks.menu.MenuBook",
@@ -14,7 +15,6 @@ local modules = {
     "BeefStranger.UI Tweaks.menu.MenuEnchantedGear",
     "BeefStranger.UI Tweaks.menu.EmbeddedServices",
     "BeefStranger.UI Tweaks.menu.MenuHitChance",
-    "BeefStranger.UI Tweaks.menu.MenuHotkeys",
     "BeefStranger.UI Tweaks.menu.MenuMagic",
     "BeefStranger.UI Tweaks.menu.MenuInventory",
     "BeefStranger.UI Tweaks.menu.MenuJournal",
@@ -25,7 +25,6 @@ local modules = {
     "BeefStranger.UI Tweaks.menu.MenuScroll",
     "BeefStranger.UI Tweaks.menu.MenuServices",
     "BeefStranger.UI Tweaks.menu.MenuSpellmaking",
-    "BeefStranger.UI Tweaks.menu.QuickEsc",
     "BeefStranger.UI Tweaks.modSupport.TransferEnchant",
 }
 local function requireFiles()
@@ -33,37 +32,6 @@ local function requireFiles()
         require(modname)
     end
 end
-
--- -- -- local function requireMenu(path)
--- -- --     for file in lfs.dir("Data Files/MWSE/mods/BeefStranger/UI Tweaks/menu") do
--- -- --         if file ~= "." and file ~= ".." then
--- -- --             local moduleName = string.format("BeefStranger.UI Tweaks.menu.%s", file:match("(.+)%..+$"))
--- -- --             debug.log(moduleName)
--- -- --         end
--- -- --     end
--- -- -- end
-
--- -- -- requireMenu()
-
--- -- -- local function reloadFiles()
--- -- --     for _, modname in ipairs(modules) do
--- -- --         package.loaded[string.lower(modname)] = nil
--- -- --         require(string.lower(modname))
--- -- --     end
--- -- --     tes3.messageBox("UI Tweaks: Files Reloading:")
--- -- -- end
--- -- -- event.register("UITweaksReloadFile", reloadFiles)
-
--- -- -- ---@param e keyDownEventData
--- -- -- local function onKeyDownAltU(e)
--- -- --     if e.isAltDown then
--- -- --         reloadFiles()
--- -- --         tes3.saveGame({file = "UITweaksReload", name = "UITweaksReload"})
--- -- --         tes3.loadGame("UITweaksReload.ess")
--- -- --         return false
--- -- --     end
--- -- -- end
--- -- -- event.register("keyDown", onKeyDownAltU, {filter=tes3.scanCode.u})
 
 requireFiles()
 
---
+++ quickEscape.lua
@@ -0,0 +1,91 @@
+local cfg = require("BeefStranger.UI Tweaks.config")
+local bs = require("BeefStranger.UI Tweaks.common")
+local id = require("BeefStranger.UI Tweaks.ID")
+local find = tes3ui.findMenu
+
+local closeButtons
+local closeText
+--- @param e initializedEventData
+local function initializedCallback(e)
+    closeButtons = {
+        [id.Ctrls] = "MenuCtrls_Okbutton",
+        [id.Video] = "MenuVideo_Okbutton",
+        [id.Prefs] = "MenuPrefs_Okbutton",
+        [id.Audio] = "MenuAudio_Okbutton",
+        [id.Options] = "MenuOptions_Return_container",
+        [id.Book] = "MenuBook_button_close",
+        [id.Scroll] = "MenuScroll_Close",
+        [id.Journal] = "MenuBook_button_close",
+    }
+
+    closeText = {
+        [bs.GMST(tes3.gmst.sGoodbye)] = true,
+        [bs.GMST(tes3.gmst.sCancel)] = true,
+        [bs.GMST(tes3.gmst.sClose)] = true,
+        [bs.GMST(tes3.gmst.sOK)] = true,
+        [bs.GMST(tes3.gmst.sDone)] = true,
+    }
+end
+event.register(tes3.event.initialized, initializedCallback)
+
+--- Close menus on Right click
+--- @param e keybindTestedEventData
+local function keybindTestedCallback(e)
+    if not cfg.escape.enable then return end
+    if e.keybind == cfg.escape.keybind and e.result then
+        if tes3.isCharGenRunning() then return end
+        if tes3ui.menuMode() then
+            local menu = tes3ui.getMenuOnTop()
+            if cfg.escape.blacklist[menu.name] then
+                return false
+            end
+            ---Menus that dont have "Cancel" buttons
+            for menuID, button in pairs(closeButtons) do
+                if menuID == menu.name then
+                    local child = menu:findChild(button)
+                    if child.visible then
+                        child:bs_click()
+                        return false
+                    end
+                end
+            end
+
+            ---Most Menus can be closed by findind the cancel button
+            for button in table.traverse(menu.children) do
+                ---@cast button tes3uiElement
+                if button.type == tes3.uiElementType.button then
+                    if button.visible then
+                        if closeText[button.text] then
+                            button:bs_click()
+                            return false
+                        end
+                    end
+                end
+            end
+            tes3ui.leaveMenuMode()
+            return false
+        end
+    end
+end
+event.register(tes3.event.keybindTested, keybindTestedCallback)
+
+
+---NEEDS TO MOVE TO MENUOPTIONS!!!
+---@param e uiActivatedEventData
+local function uiActivatedCallback(e)
+    local width = 400
+    local height = 500
+    if e.element.name == id.Ctrls then
+        e.element.width = width
+        e.element.height = height
+        for child in table.traverse(e.element:getContentElement().children) do ---@param child tes3uiElement
+            if child.type == tes3.uiElementType.button  then
+                if child.text == bs.GMST(tes3.gmst.sOK) then
+                    child:bs_Rename("MenuCtrls_Okbutton")
+                end
+            end
+        end
+        e.element:updateLayout()
+    end
+end
+event.register(tes3.event.uiActivated, uiActivatedCallback)
