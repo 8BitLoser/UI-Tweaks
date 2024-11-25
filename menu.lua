d83042720399b14cba04d25e922d9b1e0a75c29a
BeefStranger <8BitLoser@users.noreply.github.com>
2024-11-25 00:29:30 -0700

    Added menu.lua

---
+++ menu.lua
@@ -0,0 +1,24 @@
+---All mapped out menus
+---@class bsUITweaksMenus
+local menu = {
+    Tooltip = require("BeefStranger.UI Tweaks.menu.HelpMenu"),
+    Barter = require("BeefStranger.UI Tweaks.menu.MenuBarter"),
+    Book = require("BeefStranger.UI Tweaks.menu.MenuBook"),
+    Contents = require("BeefStranger.UI Tweaks.menu.MenuContents"),
+    Dialog = require("BeefStranger.UI Tweaks.menu.MenuDialog"),
+    Effects = require("BeefStranger.UI Tweaks.menu.MenuEffects"),
+    Enchantment = require("BeefStranger.UI Tweaks.menu.MenuEnchantment"),
+    EnchantedGear = require("BeefStranger.UI Tweaks.menu.MenuEnchantedGear"),
+    EmbeddedServices = require("BeefStranger.UI Tweaks.menu.EmbeddedServices"),
+    HitChance = require("BeefStranger.UI Tweaks.menu.MenuHitChance"),
+    Magic = require("BeefStranger.UI Tweaks.menu.MenuMagic"),
+    Inventory = require("BeefStranger.UI Tweaks.menu.MenuInventory"),
+    Journal = require("BeefStranger.UI Tweaks.menu.MenuJournal"),
+    Persuasion = require("BeefStranger.UI Tweaks.menu.MenuPersuasion"),
+    Repair = require("BeefStranger.UI Tweaks.menu.MenuRepair"),
+    RestWait = require("BeefStranger.UI Tweaks.menu.MenuRestWait"),
+    Scroll = require("BeefStranger.UI Tweaks.menu.MenuScroll"),
+    Services = require("BeefStranger.UI Tweaks.menu.MenuServices"),
+}
+
+return menu
